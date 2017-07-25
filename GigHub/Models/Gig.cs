fc522c407d3f48c67a5d20369ae11e975f1a0029
mV <matias_soad@hotmail.com>
2017-07-25 14:29:56 -0300

    implement saving a gig

--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -10,15 +10,19 @@ namespace GigHub.Models
     {
         public int Id { get; set; }
 
-        [Required]
         public ApplicationUser Artist { get; set; }
+
+        [Required]
+        public string ArtistId { get; set; }
 
         [Required]
         [StringLength(255)]
         public string Venue { get; set; }
 
-        [Required]
         public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
 
         public DateTime DateTime { get; set; }
     }
